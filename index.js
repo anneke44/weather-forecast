9a6409306149d97f1ce0057d311d70ad4cd3f2c2
Anneke <122925139+anneke44@users.noreply.github.com>
2025-03-06 17:52:17 +0200

    Added search function using SheCodes API

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temp-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#weather-city");
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+}
+
 function searchCity(city) {
   let apiKey = "04f3tf2c9f9bboc83b5050dcf54e2f1a";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Cape Town");
