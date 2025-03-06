d7b967d837e4e51b5d2a7b277c4a855d4b694a96
Anneke <122925139+anneke44@users.noreply.github.com>
2025-03-06 17:15:52 +0200

    Added API key and URL

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "04f3tf2c9f9bboc83b5050dcf54e2f1a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
