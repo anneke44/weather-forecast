3df178c608af6b051bdbd939bfcf294ac4b3e43a
Anneke <122925139+anneke44@users.noreply.github.com>
2025-03-06 18:52:45 +0200

    Injected weather data

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temp-value");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#weather-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#day-time");
+  let date = new Date(response.data.time * 1000);
 
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
 }
 
 function searchCity(city) {
@@ -17,6 +26,30 @@ function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   searchCity(searchInput.value);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let days = day[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+  }
+  return `${days} ${hours}:${minutes}`;
 }
 
 let searchFormElement = document.querySelector("#search-form");
